b5716e6d1791561d99126280fdd855529343be6d
SpyingEnvelope <gadi800@gmail.com>
2022-01-02 10:02:42 -0700

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -45,5 +45,52 @@ const generateSvg = () => {
     let cells = svg.selectAll('g')
                    .data(vgLeaves)
                    .enter()
-                   .append('g');
+                   .append('g')
+                   .attr('transform', (d) => `translate(${d.x0}, ${d.y0})`)
+                   .append('rect')
+                   .attr('class','tile')
+                   .attr('fill', (d) => tileFiller(d.data.category))
+                   .attr('data-name', (d) => d.data.name)
+                   .attr('data-category', (d) => d.data.category)
+                   .attr('data-value', (d) => d.data.value)
+                   .attr('datapoint', (d) => console.log(d))
+}
+
+const tileFiller = (name) => {
+    switch (name) {
+        case 'Wii':
+            return '#FF7F50';
+        case 'X360':
+            return '#00FFFF';
+        case 'XOne':
+            return '#F0F8FF';
+        case 'PSP':
+            return '#A52A2A';
+        case 'XB':
+            return '#7FFFD4';
+        case 'PS':
+            return '#DEB8887';
+        case 'N64':
+            return '#8B008B';
+        case '3DS':
+            return '#00FF00';
+        case 'PS4':
+            return '#D2691E';
+        case 'GBA':
+            return '#7CFC00';
+        case 'SNES':
+            return '#D3D3D3';
+        case 'PS2':
+            return '#BDB76B';
+        case 'PS3':
+            return '#B8860B';
+        case 'DS':
+            return '#32CD32';
+        case 'GB':
+            return 'green';
+        case 'NES':
+            return '#708090';
+        case '2600':
+            return '#4682B4';
+    }
 }
